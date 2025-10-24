844f51cb56d5ac4a94d5b566d242140b264747fa
Victor Hernández Salcedo <victorhernandezsalcedo4@gmail.com>
2025-10-24 12:10:35 -0400

    modelo de usuario de mongo en infraestructura

---
+++ src/Infrastructure/Database/Models/UserMongo.cs
@@ -0,0 +1,21 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CMS_NetApi.Infrastructure.Database.Models;
+
+public class UserMongo
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    public string Nombre { get; set; } = null!;
+  
+    public string Email { get; set; } = null!;
+
+    
+    public string PasswordHash { get; set; } = null!;
+    public string? Telefono { get; set; }
+    public string TipoUsuario { get; set; } = "Sin Asignar";
+    public string? RelacionId { get; set; }
+}
