7cc90aa26e0cb64245714fe37d37daa07f1326e4
Victor Hernández Salcedo <victorhernandezsalcedo4@gmail.com>
2025-10-24 12:07:24 -0400

    ajustes de jwt

---
+++ src/Application/Settings/JwtSettings.cs
@@ -0,0 +1,9 @@
+namespace CMS_NetApi.Application.Settings;
+
+public class JwtSettings
+{
+    public string SecretKey { get; set; } = null!;
+    public string Issuer { get; set; } = null!;
+    public string Audience { get; set; } = null!;
+    public int ExpirationInMinutes { get; set; }
+}
