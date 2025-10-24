17f84967fd3979a481d44a8d4fd5d22bdbe6a42c
Victor Hernández Salcedo <victorhernandezsalcedo4@gmail.com>
2025-10-24 12:10:59 -0400

    repositorio de usuario donde se implementa la interacion con la db

---
+++ src/Infrastructure/Repositories/UsuarioRepository.cs
@@ -0,0 +1,50 @@
+using CMS_NetApi.Domain.Entities;
+using CMS_NetApi.Domain.Interfaces;
+using CMS_NetApi.Infrastructure.Database.Models;
+using CMS_NetApi.Infrastructure.Database.Context;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using AutoMapper;
+
+namespace CMS_NetApi.Infrastructure.Repositories;
+
+public class UsuarioRepository(MongoDbContext context, IMapper mapper) : IUsuarioRepository
+{
+    private readonly IMongoCollection<UserMongo> _user = context.Usuarios;
+    private readonly IMapper _mapper = mapper;
+    
+
+    public async Task<User?> GetByEmailAsync(string email, CancellationToken ct = default)
+    {
+        var mongo = await _user.Find(x => x.Email == email)
+                              .FirstOrDefaultAsync(ct);
+        return mongo is null ? null : _mapper.Map<User>(mongo);
+    }
+
+    public async Task<User?> GetByIdAsync(string id, CancellationToken ct = default)
+    {
+        var mongo = await _user.Find(x => x.Id == id)
+                              .FirstOrDefaultAsync(ct);
+        return mongo is null ? null : _mapper.Map<User>(mongo);
+    }
+
+    public async Task AddAsync(User usuario, CancellationToken ct = default)
+    {
+        var mongo = _mapper.Map<UserMongo>(usuario);
+
+        if (string.IsNullOrEmpty(mongo.Id))
+        mongo.Id = ObjectId.GenerateNewId().ToString();
+        await _user.InsertOneAsync(mongo, cancellationToken: ct);
+    }
+
+    public async Task UpdateAsync(User usuario, CancellationToken ct = default)
+    {
+        var mongo = _mapper.Map<UserMongo>(usuario);
+        await _user.ReplaceOneAsync(x => x.Id == mongo.Id, mongo, cancellationToken: ct);
+    }
+
+    public async Task DeleteAsync(string id, CancellationToken ct = default)
+    {
+        await _user.DeleteOneAsync(x => x.Id == id, cancellationToken: ct);
+    }
+}
