7d0e6fccb6524c52c4d70b793e8fef7647a4641a
Victor Hernández Salcedo <victorhernandezsalcedo4@gmail.com>
2025-10-24 12:08:18 -0400

    extencion para el swager, documetacion de api

---
+++ src/Presentation/Extensions/SwaggerExtensions.cs
@@ -0,0 +1,44 @@
+using Microsoft.OpenApi.Models;
+
+namespace CMS_NetApi.Presentation.Extensions;
+
+public static class SwaggerExtensions
+{
+    public static IServiceCollection AddSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new() { Title = "CMS API", Version = "v1" });
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                Scheme = "bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header
+            });
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
+        });
+        return services;
+    }
+
+    public static IApplicationBuilder UseSwaggerMiddleware(this IApplicationBuilder app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CMS API v1"));
+        return app;
+    }
+}
