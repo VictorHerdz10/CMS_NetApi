ecc4f3d2097c0207955430bba21d5d78b83812c7
Victor Hernández Salcedo <victorhernandezsalcedo4@gmail.com>
2025-10-30 00:19:02 -0400

    agregada documentacon al response de auth

--- src/Presentation/Dtos/Responses/AuthResponseDto.cs
+++ src/Presentation/Dtos/Responses/AuthResponseDto.cs
@@ -1,8 +1,14 @@
-
-namespace CMS_NetApi.Presentation.Dtos.Request
+namespace CMS_NetApi.Presentation.Dtos.Responses
 {
+    /// <summary>
+    /// DTO para respuesta de autenticación
+    /// </summary>
     public class AuthResponse
     {
+        /// <summary>
+        /// Token JWT generado para el usuario
+        /// </summary>
+        /// <example>eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...</example>
         public string Token { get; set; } = null!;
     }
 }
